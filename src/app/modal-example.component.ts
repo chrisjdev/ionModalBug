d0a49d5818cd0012c7f7cac6ebc5ceb96caa4fc6
Chris J <chrisjdev@users.noreply.github.com>
2025-01-21 21:44:20 -0500

    Add files via upload

---
+++ src/app/modal-example.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonTitle,
+  IonToolbar,
+  ModalController,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-modal-example',
+  templateUrl: 'modal-example.component.html',
+  imports: [FormsModule, IonButton, IonButtons, IonContent, IonHeader, IonInput, IonFooter, IonItem, IonTitle, IonToolbar],
+})
+export class ModalExampleComponent {
+  name!: string;
+
+  constructor(private modalCtrl: ModalController) { }
+
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    return this.modalCtrl.dismiss(this.name, 'confirm');
+  }
+}
